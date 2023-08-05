61ed7ec0a52e3830b141d2366c902a867f34b359
MCPro2019 <71758148+mcpro2019@users.noreply.github.com>
2023-08-06 01:48:52 +0900

    rss

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,6 +4,8 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
+// https://helios-files.geekcorner.eu.org/distribution.json
+// https://raw.githubusercontent.com/mcpro2019/TMCLauncher/main/distribution.json
 exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/mcpro2019/TMCLauncher/main/distribution.json'
 
 const api = new DistributionAPI(

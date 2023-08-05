4f37bf3d76e33d505ec9b7c53ec117f39a0fe020
MCPro2019 <71758148+mcpro2019@users.noreply.github.com>
2023-08-06 02:33:13 +0900

    builder

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -6,6 +6,7 @@ const ConfigManager = require('./configmanager')
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
 // https://helios-files.geekcorner.eu.org/distribution.json
 // https://raw.githubusercontent.com/mcpro2019/TMCLauncher/main/distribution.json
+// https://firebasestorage.googleapis.com/v0/b/themarscity-mc.appspot.com/o/distribution.json?alt=media&token=19e8a63a-6296-4aac-9ada-c02d8b869ca9
 exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/mcpro2019/TMCLauncher/main/distribution.json'
 
 const api = new DistributionAPI(
